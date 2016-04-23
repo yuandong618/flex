1509d2362fda8d18f82eb87c8947dd2565dc2de4
yuandong <yuandong618@gmail.com>
2016-04-23 17:17:12 +0800

    add wrap

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-
+var wrap = require('gulp-wrap');
 function handleError(err)
 {
 console.log(err.toString());
 this.emit('end');
 }
+
+gulp.task('build',function(){
+	gulp.src('pages/*.html')
+	.pipe(wrap({src:"layout/default.html"}))
+	.pipe(gulp.dest('..'));
+
+});
 
 gulp.task('sass',function (){
 	gulp.src('styles/main.scss')
@@ -16,14 +23,10 @@ gulp.task('sass',function (){
 		.pipe(gulp.dest('../styles'));
 });
 
-gulp.task('cp',function(){
-	gulp.src('index.html')
-		.pipe(gulp.dest('..'));
-});
 
 gulp.task('watch',function(){
-	gulp.watch(['*.html'],['cp']);
+	gulp.watch(['**/*.html'],['build']);
 	gulp.watch(['styles/*.scss'],['sass']);
 });
 
-gulp.task('default',['sass','cp','watch']);
+gulp.task('default',['sass','build','watch']);
